01728acf655177b75af221b22cbaa41dec52016b
Maddy Tan <maddytan7@gmail.com>
2022-05-11 18:00:23 +0000

    added connection to input file to write commands using connection object

--- input.js
+++ input.js
@@ -1,18 +1,32 @@
+let connection;
+
 // create a function to register user input as data for stdin
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   };
+
+  if (key === '\x77'){
+    connection.write("Move: up")
+  } else if (key === '\x61'){
+    connection.write("Move: left")
+  } else if (key === '\x73'){
+    connection.write("Move: down")
+  } else if (key === '\x64'){
+    connection.write("Move: right");
+  };
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding("utf8");
-stdin.resume();
-stdin.on("data", handleUserInput);
-return stdin;
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
 };
 
 module.exports = setupInput;
+
